c69a7d50f9337bf723af79738b8e4a340f861520
ailtonbsj <ailton.ifce@gmail.com>
2019-07-18 16:12:40 -0300

    Revision 4 from 22/09/2013

--- Sources/ProcessorExpert.c
+++ Sources/ProcessorExpert.c
@@ -4,7 +4,7 @@
  **     Processor   : MKL25Z128VLK4
  **     Version     : Driver 01.01
  **     Compiler    : GNU C Compiler
- **     Date/Time   : 2013-09-12, 20:37, # CodeGen: 0
+ **     Date/Time   : 2013-09-18, 19:23, # CodeGen: 0
  **     Abstract    :
  **         Main module.
  **         This module contains user's application code.
@@ -33,28 +33,26 @@
 #include "BitIoLdd1.h"
 #include "CameraSI1.h"
 #include "BitIoLdd2.h"
-#include "Analog1.h"
-#include "AdcLdd1.h"
-#include "Serial1.h"
-#include "ASerialLdd1.h"
-#include "CameraTimer.h"
+#include "CameraTimer1.h"
 #include "TimerIntLdd1.h"
 #include "TU1.h"
-#include "Servo1.h"
+#include "Analog1.h"
+#include "AdcLdd1.h"
+#include "TracaoA1PWM.h"
 #include "PwmLdd1.h"
 #include "TU2.h"
-#include "Tracao1.h"
+#include "TracaoA2.h"
 #include "BitIoLdd3.h"
-#include "Tracao2.h"
+#include "TracaoB1.h"
 #include "BitIoLdd4.h"
-#include "TracaoPWM1.h"
+#include "TracaoB2.h"
+#include "BitIoLdd5.h"
+#include "TracaoEnable.h"
+#include "BitIoLdd6.h"
+#include "Servo1.h"
 #include "PwmLdd2.h"
 #include "TU3.h"
-#include "TracaoEnable.h"
-#include "BitIoLdd5.h"
 #include "SW1.h"
-#include "BitIoLdd6.h"
-#include "Tracao3.h"
 #include "BitIoLdd7.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
@@ -75,14 +73,9 @@ unsigned long menorAmostra = 65535;
 
 /* Modulo Servo */
 unsigned long tempoDuty = 18700;
-int detLinePos = 0;
-int detLineNeg = 0;
-int detectLine = 0;
-int servoI = 18450;
-int servoE = 18950;
-
-/* Modulo Controle e opções */
-long velocidade = 1;
+short detFirstLine = 0;
+short detSeconLine = 0;
+short detectLine = 0;
 
 /* funcao Delay */
 void delay(int valor) {
@@ -103,95 +96,57 @@ int main(void)
 
 	/* Write your code here */
 	/* For example: for(;;) { } */
-	/**/
-	//Serial1_Disable();
-	/**/
+
+	/* Modulo de Acionamento de Componentes */
 	Analog1_Start();
+
 	/* Modulo Tracao */
 	TracaoEnable_PutVal(1);
-	Tracao1_PutVal(0);
-	Tracao2_PutVal(0);
-	Tracao3_PutVal(0);
-	TracaoPWM1_SetDutyUS(19000);
+	TracaoA2_PutVal(0);
+	TracaoB1_PutVal(0);
+	TracaoB2_PutVal(0);
+	TracaoA1PWM_SetDutyUS(13000);
 
 	while (TRUE) {
-		short cont;
+		int cont;
+
 		/* Modulo Camera */
 		if (cameraFinished == 1) {
 			cameraFinished = 0;
 
 			for (cont = 18; cont <= 109; cont++) {
 				if ((((double) 4 / (maiorAmostra - menorAmostra))
-						* (linhaBruta[cont] - menorAmostra)) <= 1) {
+						* (linhaBruta[cont] - menorAmostra)) <= 2) {
 					linha[cont - 18] = 0;
 				} else {
 					linha[cont - 18] = 1;
 				}
-				//linha[cont - 18] = (((double) 4 / (maiorAmostra - menorAmostra)) * (linhaBruta[cont] - menorAmostra));
 			}
+
+			/* Modulo servo Motor */
+			/* Tempo usando atualmente 18400--18700--19000 */
+			for (cont = 0; cont <= 89; cont++) {
+				if ((linha[cont] == 0) && (linha[cont + 1] == 0)
+						&& (linha[cont + 2] == 0)) {
+					detectLine = cont + 1;
+					break;
+				}
+			}
+			
+			
+			Servo1_SetDutyUS((6.66666 * (90 - detectLine)) + 18400);
 
 			maiorAmostra = 0;
 			menorAmostra = 65535;
 			Analog1_Enable();
 			Analog1_Start();
-			CameraTimer_Enable();
-
-			/* Modulo servo Motor */
-			//TEMPO = 18.45ms  18.7ms   18.95ms    18450-18950
-			for (cont = 0; cont <= 89; cont++) {
-				if (linha[cont] == 0 && linha[cont + 1] == 0
-						&& linha[cont + 2] == 0) {
-					detLinePos = cont;
-					break;
-				}
-			}
-			for (cont = 91; cont >= 2; cont--) {
-				if (linha[cont] == 0 && linha[cont - 1] == 0
-						&& linha[cont - 2] == 0) {
-					detLineNeg = cont;
-					break;
-				}
-			}
-			detectLine = ((detLineNeg-detLinePos)/2)+detLinePos;
-
-			Servo1_SetDutyUS(((double)(90-detectLine)*((double)(servoE-servoI)/90))+servoI);
-
-			/* Modulo Controle e opções */
-			if (SW1_GetVal() == 1) {
-				TracaoPWM1_SetDutyUS(12000);
-			}
-
-			/* Modulo Sinal via Serial
-			for (cont = 0; cont <= 91; cont++) {
-				switch (linha[cont]) {
-				case 0:
-					Serial1_SendChar('0');
-					break;
-				case 1:
-					Serial1_SendChar('1');
-					break;
-				case 2:
-					Serial1_SendChar('2');
-					break;
-				case 3:
-					Serial1_SendChar('3');
-					break;
-				case 4:
-					Serial1_SendChar('4');
-					break;
-				case 5:
-					Serial1_SendChar('5');
-					break;
-
-				}
-				delay(3000);
-			}
-			Serial1_SendChar('\n');
-			delay(3000);
-			Serial1_SendChar('\r');
-			delay(3000);
-			 FIM Modulo Sinal via Serial */
+			CameraTimer1_Enable();
 		}
+
+		/* Modulo Controle e opções 
+		 if (SW1_GetVal() == 1) {
+		 TracaoA1PWM_SetDutyUS(16000); //12000
+		 } */
 
 	}
 
